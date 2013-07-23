11626c38db2b7c8173bab68be1b616ee947b24f6
Hedrick David <hedrick@klab.com>
2013-07-23 13:29:05 +0800

    Initial Skeleton

---
+++ Client/Algorithms/Assets/Scripts/Enum/AlgorithmType.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public enum AlgorithmType {
+	MinMax,
+	QuickSort,
+	MergeSort,
+	HeapSort,
+	CountingSort,
+	RadixSort,
+	BucketSort,
+	SortingNetworks,
+	BitonicSortingOrder,
+	MergingNetworks,
+	AmortizedAnalysis,
+	StoogeSort
+}
---
+++ Client/Algorithms/Assets/Scripts/MainApplication.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainApplication : MonoBehaviour {
+
+	[SerializeField]
+	private AlgorithmType algorithmType;
+
+	// Use this for initialization
+	void Start () {
+		StartApplication ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void StartApplication(){
+
+	}
+}
